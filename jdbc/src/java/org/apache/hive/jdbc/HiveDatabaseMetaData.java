3f14385120d8fe9b2bebc6a03f1e8109534c1ecf
RomaZe <rzolotov@arenadata.io>
2021-11-09 16:25:06 +0300

    AIS-107: Change quota character returned by getIdentifierQuoteString()

--- jdbc/src/java/org/apache/hive/jdbc/HiveDatabaseMetaData.java
+++ jdbc/src/java/org/apache/hive/jdbc/HiveDatabaseMetaData.java
@@ -365,7 +365,7 @@ public class HiveDatabaseMetaData implements DatabaseMetaData {
   }
 
   public String getIdentifierQuoteString() throws SQLException {
-    return " ";
+    return "`";
   }
 
   public ResultSet getImportedKeys(String catalog, String schema, String table)
