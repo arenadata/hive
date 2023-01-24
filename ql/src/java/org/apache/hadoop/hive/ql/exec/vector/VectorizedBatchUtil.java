503c7f88b2e64642fe4df599939c619af329537e
Andrey Shitov <sai@arenadata.io>
2023-01-24 14:43:11 +0400

    HIVE-22540

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizedBatchUtil.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizedBatchUtil.java
@@ -589,7 +589,7 @@ public class VectorizedBatchUtil {
           decColVector.scale);
     } else if (source instanceof Decimal64ColumnVector) {
         Decimal64ColumnVector dec64ColVector = (Decimal64ColumnVector) source;
-        return new DecimalColumnVector(dec64ColVector.vector.length,
+        return new Decimal64ColumnVector(dec64ColVector.vector.length,
             dec64ColVector.precision,
             dec64ColVector.scale);
     } else if (source instanceof TimestampColumnVector) {
