73b6290b6f6a3e2efe81cf33e4efe122a4051296
Vitaliy Dmitriev <v.dmitriev@arenadata.io>
2025-11-06 17:26:49 +0300

    ADH-6957: removed unnecessary reset(mockedAuthorizer) in test

--- ql/src/test/org/apache/hadoop/hive/ql/security/authorization/plugin/TestHivePrivilegeObjectOwnerNameAndType.java
+++ ql/src/test/org/apache/hadoop/hive/ql/security/authorization/plugin/TestHivePrivilegeObjectOwnerNameAndType.java
@@ -187,7 +187,6 @@ public class TestHivePrivilegeObjectOwnerNameAndType {
   public void testSelectSingleInstanceOfHPOForPartitionedTable() throws Exception {
       runCmd("CREATE EXTERNAL TABLE Part2 (eid int, name int) PARTITIONED BY (position int, dept int, sal int)");
 
-      reset(mockedAuthorizer);
       runCmd("insert overwrite table part2 partition(position=2,DEPT,SAL) select 2,2,2,2");
       runCmd("insert overwrite table part2 partition(position=3,DEPT,SAL) select 3,3,3,3");
       reset(mockedAuthorizer);
